6eb4e3382139002f149c576ceda87708ee241ca6
Tejas H S <tejas@Gateway-to-Hell.local>
2016-11-05 19:35:02 -0600

    Fixing the initial words. Adding more phrase tags. they were missing. Hence was not entering loop.

--- src/main/java/ga/coreference/main/POSUtility.java
+++ src/main/java/ga/coreference/main/POSUtility.java
@@ -10,24 +10,44 @@ public class POSUtility {
     private static ArrayList<String> nounRelatedTags;
     static{
         phrases = new ArrayList<String>();
+        //CLAUSES as Per PENN TREEBANK
         phrases.add("S");
         phrases.add("SBAR");
         phrases.add("SBARQ");
         phrases.add("SINV");
         phrases.add("SQ");
-        phrases.add("NP");
-        phrases.add("VP");
-        phrases.add("PP");
+        //PHRASES as per PENN TREEBANK
         phrases.add("ADJP");
         phrases.add("ADVP");
         phrases.add("CONJP");
+        phrases.add("FRAG");
+        phrases.add("INTJ");
+        phrases.add("LST");
+        phrases.add("NAC");
+        phrases.add("NP");
+        phrases.add("NX");
+        phrases.add("PP");
+        phrases.add("PRN");
+        phrases.add("PRP");
+        phrases.add("QP");
+        phrases.add("RRC");
+        phrases.add("UCP");
+        phrases.add("VP");
+        phrases.add("WHADJP");
+        phrases.add("WHAVP");
+        phrases.add("WHNP");
+        phrases.add("WHPP");
+        phrases.add("X");
+
 
         nounRelatedTags = new ArrayList<String>();
         nounRelatedTags.add("NP");
         nounRelatedTags.add("NN");
+        nounRelatedTags.add("NX");
         nounRelatedTags.add("NNS");
         nounRelatedTags.add("NNP");
         nounRelatedTags.add("NNPS");
+        nounRelatedTags.add("WHNP");
     }
 
     public static boolean isTerminalTag(String tag){
